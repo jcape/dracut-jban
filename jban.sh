3cca0399350da2e6c9a6095768dbd1f75846ee1b
James Cape <jcape@ignore-your.tv>
2012-12-06 17:53:46 +0000

    Refactor to remove the call to the wipe command, and run the SATA commands in parallel.

--- jban.sh
+++ jban.sh
@@ -6,6 +6,21 @@
 # them. 
 #
 
+wantsuspend=""
+devices=""
+
+function sata_wipe() {
+	# If the device is locked, assume we locked it.
+	if [ -z "`hdparm -I  | grep 'not locked'`" ]; then
+		hdparm --user-master u --security-unlock jban "$device"
+		hdparm --user-master u --security-disable jban "$device"
+	fi
+
+	hdparm --user-master u --security-set-pass jban "$device"
+	hdparm --user-master u --security-erase-enhanced jban "$device"
+}
+
+# Get a list of valid block devices
 for device in /sys/block/*; do
 	target=`readlink -f "$device"`
 	if [ -n "`echo "$target" | grep '^/sys/devices/virtual/'`" ]; then
@@ -13,34 +28,35 @@ for device in /sys/block/*; do
 	fi
 
 	device="/dev/`basename $device`"
-	hdpout="`hdparm -I $device`"
-	if [ -n "`echo -e "$hdpout" | grep 'CD-ROM'`" ]; then
+	# Skip CD drives
+	if [ -n "`echo -e "`hdparm -I $device`" | grep 'CD-ROM'`" ]; then
 		continue
 	fi
 
-	echo "Attempting to use SATA commands to securely erase $device"
+	devices="$device $devices"
+done
 
-	# If the device is marked as frozen, suspend (requires external
-	# intervention to wake and continue nuking).
-	if [ -z "`echo -e "$hdpout" | grep 'not frozen'`" ]; then
-		echo "ERROR: Could not wipe because 'not frozen' was not found in the hdparm output."
-		echo -e "$hdpout"
-	fi
-
-	# Assume if it's locked that we're the one that locked it
-	if [ -z "`echo -e "$hdpout" | grep 'not locked'`"]; then
-		hdparm --user-master u --security-unlock jban "$device"
-		hdparm --user-master u --security-disable jban "$device"
-	fi
-
-	hdparm --user-master u --security-set-pass jban "$device"
-	hdparm --user-master u --security-erase-enhanced jban "$device"
-	echo "Done"
-
-	if [ -z "`echo -e "$hdpout" | grep -i 'SSD'`" ]; then
-		echo "Using wipe command against $device"
-		wipe -kDf "$device"
-		echo "Done"
+for device in $devices; do
+	if [ -z "`hdparm -I $device | grep 'not frozen'`" ]; then
+		wantsuspend="$device $wantsuspend"
 	fi
 done
 
+if [ 0 -lt $wantsuspend ]; then
+	echo
+	echo "The following devices are locked:"
+	for $device in $wantsuspend; do
+		echo "    $device"
+	done
+	echo
+	echo "Press Return to suspend to RAM. After suspending, wake the"
+	echo "computer to wipe these devices..."
+	echo
+	read
+
+	echo 'mem' > /sys/power/state
+fi
+
+for device in $devices; do
+	sata_wipe $device &
+done
